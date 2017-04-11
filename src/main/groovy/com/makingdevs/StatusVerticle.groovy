cc28d8692caede420576fccb046ce8d9582c706b
carlogilmar <carlogilmar12@gmail.com>
2017-04-11 00:03:09 -0500

    Agregando fecha y hora del envío del mensaje

--- src/main/groovy/com/makingdevs/StatusVerticle.groovy
+++ src/main/groovy/com/makingdevs/StatusVerticle.groovy
@@ -1,6 +1,5 @@
 package com.makingdevs
 
 vertx.eventBus().consumer("com.makingdevs.status"){ message ->
-  def text = message.body()
-  println text
+  println "${new Date().toLocaleString()} Maquila Status: ${message.body()}"
 }
