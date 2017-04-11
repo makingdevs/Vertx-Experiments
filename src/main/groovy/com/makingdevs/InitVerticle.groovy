182ba5690fd4322fd175e95826f9e39b760cbab9
carlogilmar <carlogilmar12@gmail.com>
2017-04-11 00:02:06 -0500

    Agregando headers del path del fichero de texto

--- src/main/groovy/com/makingdevs/InitVerticle.groovy
+++ src/main/groovy/com/makingdevs/InitVerticle.groovy
@@ -7,6 +7,10 @@ vertx.deployVerticle("com/makingdevs/ProcessorVerticle.groovy", options)
 vertx.deployVerticle("com/makingdevs/ClientWsCardVerticle.groovy", options)
 vertx.deployVerticle("com/makingdevs/StatusVerticle.groovy")
 
+def headers = [ headers: [
+                          "path" : "/home/carlogilmar/Desktop/Github/medios-pago-maquila/"
+                         ]]
+
 vertx.setPeriodic(2000){ v ->
-	vertx.eventBus().send("com.makingdevs.reader", "any")
+	vertx.eventBus().send("com.makingdevs.reader", "Leyendo fichero de texto.", headers)
 }
