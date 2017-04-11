fa4435d4100ef3eedba2a9ad084c257063e7abd6
carlogilmar <carlogilmar12@gmail.com>
2017-04-11 01:43:38 -0500

    Agregando condición de éxito para mover direcrtorio

--- src/main/groovy/com/makingdevs/ReaderVerticle.groovy
+++ src/main/groovy/com/makingdevs/ReaderVerticle.groovy
@@ -3,21 +3,25 @@ package com.makingdevs
 vertx.eventBus().consumer("com.makingdevs.reader"){ message ->
 
   def path = message.headers()["path"]
+	vertx.fileSystem().exists(path+"file.txt"){ result ->
 
-	vertx.fileSystem().exists(path+"file.txt", { result ->
 		if (result.succeeded() && result.result()) {
 			vertx.eventBus().send("com.makingdevs.status", "El archivo existe!")
 			vertx.eventBus().send("com.makingdevs.processor", path)
 		} else {
 			vertx.eventBus().send("com.makingdevs.status", "No existe el archivo")
 		}
-	})
+	}
 
 }
 
 
 vertx.eventBus().consumer("com.makingdevs.move"){ message ->
-	vertx.fileSystem().move(message.body(), "${message.body()}processed.txt", {})
-  vertx.eventBus().send("com.makingdevs.status", "Cambiando nombre del archivo y moviendo de directorio.")
+	vertx.fileSystem().move("${message.body()}file.txt", "${message.body()}processed.txt"){ response ->
+    if (response.succeeded())
+      vertx.eventBus().send("com.makingdevs.status", "Cambio nombre del archivo y moviendo de directorio.")
+    else
+      vertx.eventBus().send("com.makingdevs.status", "No se pudo mover el archivo al directorio.")
+  }
 }
 
