6b21c9411ab154a9c34bfcfcef7d0656ccbc129b
carlogilmar <carlogilmar12@gmail.com>
2017-04-12 17:31:15 -0500

    Agregando monitor verticle para clientWs

--- src/main/groovy/com/makingdevs/ClientWsCardVerticle.groovy
+++ src/main/groovy/com/makingdevs/ClientWsCardVerticle.groovy
@@ -16,6 +16,7 @@ vertx.eventBus().consumer("com.makingdevs.each.card"){ message ->
 			def card = cardsIds.get(params.line)
 			cardsIds.put(params.line, card + [cvv1: r1.result().body()])
 			//vertx.eventBus().send("com.makingdevs.card.coordinator", params)
+			vertx.eventBus().send("com.makingdevs.card.monitor", params)
 		}
     else{
 			vertx.eventBus().send("com.makingdevs.status", "Error al invocar llamada a webservice")
@@ -27,6 +28,7 @@ vertx.eventBus().consumer("com.makingdevs.each.card"){ message ->
 			def card = cardsIds.get(params.line)
 			cardsIds.put(params.line, card + [cvv2: r2.result().body()])
 			//vertx.eventBus().send("com.makingdevs.card.coordinator", params)
+			vertx.eventBus().send("com.makingdevs.card.monitor", params)
 		}else{
 			vertx.eventBus().send("com.makingdevs.status", "Error al invocar llamada a webservice")
     }
@@ -36,7 +38,8 @@ vertx.eventBus().consumer("com.makingdevs.each.card"){ message ->
     if(r3.succeeded()){
 			def card = cardsIds.get(params.line)
 			cardsIds.put(params.line, card + [cvv3: r3.result().body()])
-			vertx.eventBus().send("com.makingdevs.card.coordinator", params)
+			//vertx.eventBus().send("com.makingdevs.card.coordinator", params)
+			vertx.eventBus().send("com.makingdevs.card.monitor", params)
 		}
     else{
 			vertx.eventBus().send("com.makingdevs.status", "Error al invocar llamada a webservice")
@@ -45,6 +48,19 @@ vertx.eventBus().consumer("com.makingdevs.each.card"){ message ->
 }
 
 
+vertx.eventBus().consumer("com.makingdevs.card.monitor"){ message ->
+    //revisar si el mapa de cvvs ya contiene todos los valores para ir al coordinator
+	def params = message.body()
+	def card = cardsIds.get(params.line)
+
+    if(card.every { k, v -> v != null }){
+        vertx.eventBus().send("com.makingdevs.status", "El mapa de ${params.line} ya esta lleno, invocando al coordinator")
+	    vertx.eventBus().send("com.makingdevs.card.coordinator", params)
+    }else{
+        vertx.eventBus().send("com.makingdevs.status", "Llenando mapa de cvvs de ${params.line}")
+
+    }
+}
 
 vertx.eventBus().consumer("com.makingdevs.card.coordinator"){ message ->
 	def params = message.body()
