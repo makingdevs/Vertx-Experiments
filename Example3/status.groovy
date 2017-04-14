0dd8cabeee269abcc34cde080876ae653375a8cf
carlogilmar <carlogilmar12@gmail.com>
2017-04-14 02:42:49 -0500

    Comunicando con web

--- Example3/status.groovy
+++ Example3/status.groovy
@@ -1,5 +1,6 @@
 println "[ok] Status"
 vertx.eventBus().consumer("com.makingdevs.status"){ message ->
   println "Status Verticle: ${new Date().toLocaleString()} ::::${message.body()}::::"
+  vertx.eventBus().send("com.makingdevs.web.monitor", message.body())
 }
 
--- Example3/webroot/js/webVerticle.js
+++ Example3/webroot/js/webVerticle.js
@@ -1,6 +1,6 @@
-var eb = new EventBus('http://localhost:8000/eventbus');
+var eb = new EventBus('http://localhost:8080/eventbus');
 eb.onopen = function() {
-  eb.registerHandler('com.makingdevs.email.success', function(error, message) {
+  eb.registerHandler('com.makingdevs.web.monitor', function(error, message) {
         console.log("Verticle message"+message.body);
     });
 }
