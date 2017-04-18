f38acab06832fc30e6e639692097ab4df374180c
carlogilmar <carlogilmar12@gmail.com>
2017-04-18 11:21:26 -0500

    Eliminando webserver

--- Example3/webroot/js/webVerticle.js
+++
@@ -1,6 +0,0 @@
-var eb = new EventBus('http://localhost:8080/eventbus');
-eb.onopen = function() {
-  eb.registerHandler('com.makingdevs.web.monitor', function(error, message) {
-        $("#log").append(message.body+"<br>")
-    });
-}
