389265c2e74c66020ee74c9967fa6e8fa2b5e627
carlogilmar <carlogilmar12@gmail.com>
2017-04-20 00:44:01 -0500

    Refactorizando verticles

--- Example3/init.groovy
+++ Example3/init.groovy
@@ -1,5 +1,25 @@
-println "[ok] Init Verticle... Sending signal to reader verticle for start the process"
+import io.vertx.ext.shell.ShellService
+import io.vertx.ext.dropwizard.MetricsService
 
-def headers = [ headers: ["path" : "/home/carlogilmar/Desktop"]]
+println "[ok] Shell and metrics"
+def shell = ShellService.create(vertx, [
+  telnetOptions:[
+    host:"localhost",
+    port:3000
+    ]
+  ])
+shell.start()
 
-vertx.eventBus().send("com.makingdevs.reader", "Leyendo fichero de texto.", headers)
+def service = MetricsService.create(vertx)
+    vertx.setPeriodic(1000, { t ->
+              def metrics = service.getMetricsSnapshot(vertx.eventBus())
+                vertx.eventBus().publish("metrics", metrics)
+                })
+
+vertx.eventBus().consumer("metrics"){ message ->
+    def metrics = message.body()
+        println "*-*-"*10
+    println "Messages delivered:  "+metrics["messages.sent"]
+}
+
+
--- Example3/main.groovy
+++ Example3/main.groovy
@@ -1,8 +1,5 @@
-import io.vertx.ext.shell.ShellService
-import io.vertx.ext.dropwizard.MetricsService
-
 println "Main verticle deployed"
-
+vertx.deployVerticle("init.groovy")
 vertx.deployVerticle("checkTotal.groovy")
 vertx.deployVerticle("clientCard.groovy")
 vertx.deployVerticle("processor.groovy")
@@ -11,31 +8,12 @@ vertx.deployVerticle("monitor.groovy")
 
 vertx.deployVerticle("reader.groovy"){deploy->
     if(deploy.succeeded()){
-        vertx.deployVerticle("init.groovy")
+        println "[ok] Init Verticle... Sending signal to reader verticle for start the process"
+        def headers = [ headers: ["path" : "/home/carlogilmar/Desktop"]]
+        vertx.eventBus().send("com.makingdevs.reader", "Leyendo fichero de texto.", headers)
     }
     else{
         println "Reader Verticle cannot deploy"
     }
 }
-
-def shell = ShellService.create(vertx, [
-  telnetOptions:[
-    host:"localhost",
-    port:3000
-    ]
-  ])
-shell.start()
-
-def service = MetricsService.create(vertx)
-    vertx.setPeriodic(1000, { t ->
-              def metrics = service.getMetricsSnapshot(vertx.eventBus())
-                vertx.eventBus().publish("metrics", metrics)
-                })
-
-vertx.eventBus().consumer("metrics"){ message ->
-    def metrics = message.body()
-        println "*-*-"*10
-    println "Messages delivered:  "+metrics["messages.sent"]
-}
-
 
