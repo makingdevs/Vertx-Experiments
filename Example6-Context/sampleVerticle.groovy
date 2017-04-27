36984d05477e35d3a2e51725fac05c2557af77c8
carlogilmar <carlogilmar12@gmail.com>
2017-04-27 00:15:16 -0500

    Agregando ejemplo de context object

---
+++ Example6-Context/sampleVerticle.groovy
@@ -0,0 +1,18 @@
+vertx.eventBus().consumer("com.makingdevs.sample"){ msg ->
+
+  println "${new Date()} Sample Verticle ok"
+
+  def context = vertx.getOrCreateContext()
+
+  println context.dump()
+
+  context.put("data", "hello verticle")
+
+  context.runOnContext{ v ->
+    def hello = context.get("data")
+    println "${new Date()} Share data at context: ${hello}"
+    println "${new Date()} This will be executed asynchronously in the same context"
+  }
+}
+
+vertx.eventBus().send("com.makingdevs.sample", "message")
